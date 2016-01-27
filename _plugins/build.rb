fb35db554b8fc5a0a5b70119b1eeed260b0da715
Shawn Allen <shawn.allen@gsa.gov>
2016-01-27 14:14:16 -0800

    don't use a jekyll plugin hook at all

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -1,10 +1,8 @@
-Jekyll::Hooks.register :site, :after_reset do |site|
-    # these are the branches for which we build production assets
-    branches = ['master', 'staging', 'js-build']
+# these are the branches for which we build production assets
+branches = ['master', 'staging']
 
-    if branches.include?(ENV['BRANCH'])
-        puts '[build.rb] building for production'
-        ENV['NODE_ENV'] = 'production'
-        `npm install && npm run build > build.log`
-    end
+if branches.include?(ENV['BRANCH'])
+  puts '[build.rb] building for production'
+  ENV['NODE_ENV'] = 'production'
+  `npm install && npm run build > build.log`
 end
