8bda6bb54c2cf7b5fc4700815ce57b675ac23077
Shawn Allen <shawn.allen@gsa.gov>
2016-02-04 16:23:08 -0800

    leave off URL trailing slash

--- test/ci.sh
+++ test/ci.sh
@@ -3,14 +3,14 @@
 repo="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-  url="http://federalist.18f.gov.s3-website-us-east-1.amazonaws.com/site/${repo}/"
+  url="http://federalist.18f.gov.s3-website-us-east-1.amazonaws.com/site/${repo}"
 else
-  url="https://federalist.18f.gov/preview/${repo}/${CIRCLE_BRANCH}/"
+  url="https://federalist.18f.gov/preview/${repo}/${CIRCLE_BRANCH}"
 fi
 
 echo "testing site URL: ${url}"
 
-commit_url="${url}commit.txt"
+commit_url="${url}/commit.txt"
 commit_sha="${CIRCLE_SHA1}"
 
 echo "fetching Federalist commit data..."
