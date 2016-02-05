ae8822d4f10537bd9d5462ca81eef01f9f58e65a
Shawn Allen <shawn.allen@gsa.gov>
2016-02-04 16:28:28 -0800

    use the xunit reporter on CI

--- test/wdio.ci.js
+++ test/wdio.ci.js
@@ -6,6 +6,12 @@ config.user = process.env.SAUCE_USERNAME;
 config.key = process.env.SAUCE_ACCESS_KEY;
 config.updateJob = true;
 
+// configure the JUnit reporter for Circle CI
+config.reporter ='xunit';
+config.reporterOptions = {
+  outputDir: process.env.CIRCLE_TEST_REPORTS
+};
+
 // XXX according to the WebDriverIO docs, these two lines shouldn't be
 // necessary
 config.host = 'ondemand.saucelabs.com';
